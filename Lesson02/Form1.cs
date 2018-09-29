e806bb143bd2057959ba0831f64df54f4cb8d346
David Nemec <david.nemec@telia.no>
2018-09-29 16:36:34 +0200

    Add points rendering and normal distribution generator

--- Lesson02/Form1.cs
+++ Lesson02/Form1.cs
@@ -2,16 +2,22 @@
 using ILNumerics.Drawing.Plotting;
 using Shared.ExtensionMethods;
 using Shared.TestFunctions;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Lesson02
 {
     public partial class Form1 : Form
     {
+        private const int RenderedPointsCount = 100;
+        private const int RenderedPointDimensions = 3;
+
         private readonly ILPlotCube _plotCube;
         private readonly Dictionary<string, FunctionBase> _functions = new Dictionary<string, FunctionBase>();
         private ILSurface _currentSurface;
+        private ILPoints _points;
 
         public Form1()
         {
@@ -29,7 +35,7 @@ namespace Lesson02
         void RenderFunction()
         {
             var function = _functions[(string)functionsComboBox.SelectedItem];
-            var surface = new ILSurface((x, y) => (float)function.Calculate(x, y), xmin: -5f, xmax: 5f, ymin: -5f, ymax: 5f, xlen:100, ylen :100);
+            var surface = new ILSurface((x, y) => (float)function.Calculate(x, y), xmin: -5f, xmax: 5f, ymin: -5f, ymax: 5f, xlen: 100, ylen: 100);
 
             if (_currentSurface != null)
             {
@@ -39,7 +45,33 @@ namespace Lesson02
             _currentSurface = surface;
             _plotCube.Add(surface);
 
+            RenderPoints();
             renderContainer.Refresh();
+        }
+
+        private void RenderPoints()
+        {
+            var random = new Random();
+
+            var array = new float[RenderedPointsCount, RenderedPointDimensions];
+            foreach (var i in Enumerable.Range(0, RenderedPointsCount))
+            {
+                var (x1, x2) = random.NextUniformDistribution2D();
+                array[i, 0] = (float)x1;
+                array[i, 1] = (float)x2;
+                array[i, 2] = 1000;
+            }
+
+            if (_points != null)
+            {
+                _plotCube.Remove(_points);
+                _points.Dispose();
+            }
+
+            _points = new ILPoints();
+            _points.Positions.Update(array);
+
+            _plotCube.Add(_points);
         }
 
         private void InitFunctionsComboBox()
---
+++ Shared/ExtensionMethods/RandomExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Shared.ExtensionMethods
+{
+    public static class RandomExtensions
+    {
+        // Box-Muller Transformation (http://mathworld.wolfram.com/Box-MullerTransformation.html)
+        public static (double, double) NextNormalDistribution2D(this Random source)
+        {
+            double x1 = source.NextDouble();
+            double x2 = source.NextDouble();
+
+            double z1 = Math.Sqrt(-2 * Math.Log(x1)) * Math.Cos(2 * Math.PI * x2);
+            double z2 = Math.Sqrt(-2 * Math.Log(x1)) * Math.Sin(2 * Math.PI * x2);
+
+            return (z1, z2);
+        }
+
+        // https://blogs.sas.com/content/iml/2016/03/30/generate-uniform-2d-ball.html
+        public static (double, double) NextUniformDistribution2D(this Random source)
+        {
+            var r = Math.Sqrt(source.NextDouble());
+            var theta = source.NextDouble() * 2 * Math.PI;
+
+            var x = r * Math.Cos(theta);
+            var y = r * Math.Sin(theta);
+
+            return (x, y);
+        }
+    }
+}
