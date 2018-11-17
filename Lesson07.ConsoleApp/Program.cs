0e6c0b78382d4fbae8c29ffff7d031c64dbc9639
daywee <d4v1d.nemec@gmail.com>
2018-11-17 11:55:05 +0100

    Lesson 7: Add test console app

---
+++ Lesson07.ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson07.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iterations = 100;
+            int evolutions = 1000;
+            int populationSize = 50;
+
+            var population = new Population(CitiesSequence.GetDefaultSequence(), new GeneticAlgorithm(), populationSize);
+            var results = new List<double>();
+
+            for (int iteration = 0; iteration < iterations; iteration++)
+            {
+                population.CreateNewPopulation();
+
+                for (int evolution = 0; evolution < evolutions; evolution++)
+                {
+                    population.Evolve();
+                }
+
+                results.Add(population.BestSequence.Cost);
+
+                Console.WriteLine();
+                Console.WriteLine($"Iteration: {iteration}");
+                Console.WriteLine($"Distance: {population.BestSequence.Cost}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Best distance: {results.Min()}");
+        }
+    }
+}
